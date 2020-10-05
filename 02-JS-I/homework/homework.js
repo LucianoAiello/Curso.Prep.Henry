326bfb6a58aea9278337369a2c8001b9f54fbad9
LucianoAiello <luchoaiello1@hotmail.com>
2020-10-05 20:43:40 -0300

    homework2 terminado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -151,37 +151,46 @@ function elevar(num, exponent) {
 function redondearNumero(num) {
   // Redondea "num" y devuélvelo
   // Tu código:
-  var redondearNumero = 1.5
-  math.round(1.5)
+  var redondearNumero = Math.round(num)
   return redondearNumero;
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba y devuélvelo
   // Tu código:
+  var redondearHaciaArriba = Math.ceil(num);
+  return redondearHaciaArriba;
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  var agregarSimboloExclamacion = (str + '!');
+  return agregarSimboloExclamacion; 
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
+  var combinarNombres = nombre + ' ' + apellido;
+  return combinarNombres;
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+  var resultado = 'Hola' + ' ' + nombre + '!';
+  return resultado;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un cuadrado teniendo su altura y ancho
   // Tu código:
+  var obtenerAreaRectangulo = alto * ancho;
+  return obtenerAreaRectangulo;
 }
 
 
